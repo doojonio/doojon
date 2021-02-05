8348697b6e003f7b9a399dd2518759ef1af07e66
Anton Fedotov <anton.fedotov@rydlab.ru>
2021-02-05 23:15:46 +0500

    add(resource): realisation for resource controller

--- doojon/lib/Doojon/Controller/Resource.pm
+++ doojon/lib/Doojon/Controller/Resource.pm
@@ -1,25 +1,57 @@
 package Doojon::Controller::Resource;
 
 use Mojo::Base 'Mojolicious::Controller', -signatures;
+use Mojo::Util qw(camelize);
+
+has model => sub ($self) {$self->app->orm->resultset(camelize($self->stash('model')))};
 
 sub create ($self) {
 
-  return $self->render(json => $self->stash('model'));
+  my $obj_to_create = $self->req->json;
+  my $obj_in_db = $self->model->create($obj_to_create);
+  return $self->render(json => {$obj_in_db->get_columns});
 }
 
 sub read ($self) {
 
-  return $self->render(json => $self->stash('model'));
+  my $id = $self->param('id');
+  my $obj_in_db = $self->model->find($id);
+
+  if (!$obj_in_db) {
+    return $self->reply->not_found;
+  }
+
+  return $self->render(json => {$obj_in_db->get_columns});
 }
 
 sub update ($self) {
 
-  return $self->render(json => $self->stash('model'));
+  my $id = $self->param('id');
+  my $obj_in_db = $self->model->find($id);
+
+  if (!$obj_in_db) {
+    return $self->reply->not_found;
+  }
+
+  my $updated_fields = $self->req->json;
+  $obj_in_db->update($updated_fields);
+
+  return $self->render(json => {$obj_in_db->get_columns});
 }
 
 sub delete ($self) {
 
-  return $self->render(json => $self->stash('model'));
+  my $id = $self->param('id');
+  my $obj_in_db = $self->model->find($id);
+
+  if (!$obj_in_db) {
+    return $self->reply->not_found;
+  }
+
+  my $deleted_object_id = $obj_in_db->id;
+  $obj_in_db->delete;
+
+  return $self->render(json => $deleted_object_id);
 }
 
 1
