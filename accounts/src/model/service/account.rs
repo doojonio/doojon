94cce2c9b94706d018f4986a41135de96b36ab02
anton <tosha.fedotov.2000@gmail.com>
2021-03-23 16:30:54 +0000

    feat(accounts): password hashing during creation

--- accounts/src/model/service/account.rs
+++ accounts/src/model/service/account.rs
@@ -1,12 +1,22 @@
 use crate::model::models::{Account, NewAccount};
 use crate::model::errors::ServiceError;
+use rand_core::OsRng;
 use diesel::{self, PgConnection};
 use diesel::prelude::*;
+use pbkdf2::{Pbkdf2, password_hash::{SaltString, PasswordHasher}};
 
-pub fn create(conn: &PgConnection, account: NewAccount) -> Result<Account, ServiceError> {
+pub fn create(conn: &PgConnection, mut account: NewAccount) -> Result<Account, ServiceError> {
 
   use crate::model::schema::accounts::dsl::*;
-  let created_account = diesel::insert_into(accounts).values(account).returning((id, email, first_name, last_name, birthday)).get_result(conn)?;
+
+  let salt = SaltString::generate(&mut OsRng);
+  account.password = Pbkdf2.hash_password_simple(account.password.as_bytes(), salt.as_ref()).
+    unwrap().to_string();
+
+  let created_account = diesel::insert_into(accounts)
+    .values(account).
+    returning((id, email, first_name, last_name, birthday))
+    .get_result(conn)?;
 
   Ok(created_account)
 }
@@ -15,4 +25,7 @@ pub fn read(conn: &PgConnection, account_id: uuid::Uuid) -> Result<Account, dies
 
   use crate::model::schema::accounts::dsl::*;
   accounts.find(account_id).select((id, email, first_name, last_name, birthday)).first::<Account>(conn)
+}
+
+fn _hash_password(password: &String) {
 }
