5b027eb73fc4d34ee8fb7c9157d25c2ddd473743
anton <tosha.fedotov.2000@gmail.com>
2021-04-07 15:45:32 +0000

    feat: openapi generation for resources

--- doojon/lib/Doojon/Command/openapi.pm
+++ doojon/lib/Doojon/Command/openapi.pm
@@ -25,7 +25,7 @@ sub run ($self, $command) {
 sub cli_generate ($self) {
 
   say '✍️ processing resource routes...';
-  my @dss = map {$self->app->model->get_dataservice($_)} $self->app->model->list_dataservices;
+  my @dss = sort {$a->table cmp $b->table} map {$self->app->model->get_dataservice($_)} $self->app->model->list_dataservices;
 
   monkey_patch 'Mojo::Template::Sandbox',
     to_openapi_type => sub ($type) {
@@ -36,34 +36,159 @@ sub cli_generate ($self) {
     },
     has_id => sub ($ds) {
       !!$ds->columns->{id}
-    },;
+    },
+    ls_required_in_create => sub ($ds) {
+      sort grep {$ds->columns->{$_}{required} and not($ds->columns->{$_}{has_default})} keys $ds->columns->%*
+    },
+    ls_required => sub ($ds) {
+      sort grep {$ds->columns->{$_}{required}} keys $ds->columns->%*
+    };
 
-  my $resources_schema = $self->render_data('resources.ep', {
+  my $resources_paths = $self->render_data('resource_paths.ep', {
     dss => \@dss
   });
 
-  print $resources_schema;
+  my $paths = $self->render_data('paths.ep', {
+    resources_paths => $resources_paths
+  });
+
+  my $openapi = $self->render_data('openapi.ep', {
+    security_block => '',
+    paths_block => $paths,
+    components_block => '',
+  });
+
+  my $openapi_file = $self->app->home->child('openapi.yml');
+  say "[write] $openapi_file";
+
+  $openapi_file->spurt($openapi)
 }
 
 1
 
 __DATA__
-@@ resources.ep
+@@ openapi.ep
+openapi: 3.0.3
+info:
+  title: openapi for doojon.com
+  version: 0.0.1
+<%=$security_block%>
+<%=$paths_block%>
+<%=$components_block%>
+
+@@ paths.ep
+paths:
+<%=$resources_paths%>
+
+@@ resource_paths.ep
 % for my $ds ($dss->@*) {
   /api/resource/<%=$ds->table%>:
     post:
+      tags: [resource_<%=$ds->table%>]
+      description: create-resource for <%=$ds->table%>
       requestBody:
         content:
           application/json:
             schema:
               type: object
+              required:
+              % for my $field (ls_required_in_create($ds)) {
+                - <%=$field%>
+              % }
               properties:
+              % for my $column (sort keys $ds->columns->%*) { my $info = $ds->columns->{$column};
+                <%=$column%>:
+                  type: <%=to_openapi_type($info->{data_type})%>
+              % }
+      responses:
+        "200":
+          description: <%=has_id($ds) ? 'id of created resource' : 'prints "1" as a sign of success'%>
+          content:
+            application/json:
+              schema:
+              % if (has_id($ds)) {
+                type: object
+                required: [id]
+                properties:
+                  id:
+                    type: <%=to_openapi_type($ds->columns->{id}{data_type})%>
+              % }
+              % else {
+                enum: [1]
+              % }
   % if (has_id($ds)) {
     get:
+      tags: [resource_<%=$ds->table%>]
       parameters:
         - name: id
           in: query
           schema:
             type: <%= to_openapi_type($ds->columns->{id}{data_type})%>
+      responses:
+        "200":
+          description: fields of record of resource "<%=$ds->table%>"
+          content:
+            application/json:
+              schema:
+                type: object
+                required:
+                % for my $field (ls_required($ds)) {
+                  - <%=$field%>
+                % }
+                properties:
+                % for my $column (sort keys $ds->columns->%*) { my $info = $ds->columns->{$column};
+                  <%=$column%>:
+                    type: <%=to_openapi_type($info->{data_type})%>
+                % }
+    put:
+      tags: [resource_<%=$ds->table%>]
+      parameters:
+        - name: id
+          in: query
+          schema:
+            type: <%= to_openapi_type($ds->columns->{id}{data_type}) %>
+      requestBody:
+        content:
+          application/json:
+            schema:
+              type: object
+              required:
+              % for my $field (ls_required_in_create($ds)) {
+                - <%=$field%>
+              % }
+              properties:
+              % for my $column (sort keys $ds->columns->%*) { my $info = $ds->columns->{$column};
+                <%=$column%>:
+                  type: <%=to_openapi_type($info->{data_type})%>
+              % }
+      responses:
+        "200":
+          description: id of the updated record
+          content:
+            application/json:
+              schema:
+                type: object
+                required: [id]
+                properties:
+                  id:
+                    type: <%= to_openapi_type($ds->columns->{id}{data_type}) %>
+    delete:
+      tags: [resource_<%=$ds->table%>]
+      parameters:
+        - name: id
+          in: query
+          schema:
+            type: <%= to_openapi_type($ds->columns->{id}{data_type}) %>
+      responses:
+        "200":
+          description: id of the deleted record
+          content:
+            application/json:
+              schema:
+                type: object
+                required: [id]
+                properties:
+                  id:
+                    type: <%= to_openapi_type($ds->columns->{id}{data_type}) %>
   % }
 % }
