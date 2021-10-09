f1645dab379ba73a99312e2311dcece25f08d986
Anton Fedotov <tosha.fedotov.2000@gmail.com>
2021-10-09 16:13:48 +0000

    feat: prepare signup form

--- doojonf/src/app/authorization/authorization.module.ts
+++ doojonf/src/app/authorization/authorization.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthorizationRoutingModule } from './authorization-routing.module';
 import { SignupComponent } from './signup/signup.component';
 import { SigninComponent } from './signin/signin.component';
 
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [SignupComponent, SigninComponent],
-  imports: [CommonModule, AuthorizationRoutingModule, MatCardModule],
+  imports: [
+    CommonModule,
+    AuthorizationRoutingModule,
+    ReactiveFormsModule,
+
+    /**
+     * Angular Material
+     */
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatStepperModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
 export class AuthorizationModule {}
--- doojonf/src/app/authorization/signup/signup.component.ts
+++ doojonf/src/app/authorization/signup/signup.component.ts
@@ -1,16 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
-  minutes = 4;
+  signUpForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(formBuilder: FormBuilder) {
+    this.signUpForm = formBuilder.group({
+      email: ['', this.emailValidators],
+      password: ['', this.passwordValidators],
+      username: ['', this.usernameValidators],
+    });
   }
 
+  showPassword = false;
+
+  private emailValidators = [
+    Validators.requiredTrue,
+    Validators.email,
+    Validators.maxLength(320),
+  ];
+
+  private passwordValidators = [
+    Validators.requiredTrue,
+    Validators.minLength(8),
+    Validators.maxLength(32),
+  ];
+
+  private usernameValidators = [
+    Validators.requiredTrue,
+    Validators.minLength(3),
+    Validators.maxLength(16),
+  ];
+
+  ngOnInit(): void {}
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
 }
